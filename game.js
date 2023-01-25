cb73ea1bde98cdc9228745a5081a710d9cb01b88
randmsgames <117495369+RandmsDev@users.noreply.github.com>
2023-01-25 18:43:40 -0500

    Update game.js

--- game.js
+++ game.js
@@ -5,11 +5,6 @@ var button = document.getElementById("add-point-button");
 button.addEventListener("click", function() {
   score++;
   scoreElement.innerHTML = "Clicks: " + score;
-});
-
-button.addEventListener("click", function() {
-  score++;
-
   button.classList.add("active");
   setTimeout(function(){ button.classList.remove("active") }, 300);
 });
