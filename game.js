b0254a571fb961013cffa16e3560d3e0a65319bb
randmsgames <117495369+RandmsDev@users.noreply.github.com>
2023-01-26 23:24:44 -0500

    Add files via upload

--- game.js
+++ game.js
@@ -1,10 +1,30 @@
 var score = 0;
+var maxScore = localStorage.getItem("maxScore") || 0;
 var scoreElement = document.getElementById("score");
+var maxScoreElement = document.getElementById("max-score");
 var button = document.getElementById("add-point-button");
+var resetButton = document.getElementById("reset-button");
+
 
 button.addEventListener("click", function() {
   score++;
   scoreElement.innerHTML = "Clicks: " + score;
-  button.classList.add("active");
-  setTimeout(function(){ button.classList.remove("active") }, 300);
+  if (score > maxScore) {
+    maxScore = score;
+    maxScoreElement.innerHTML = "Max Clicks: " + maxScore;
+    localStorage.setItem("maxScore", maxScore);
+  }
 });
+
+resetButton.addEventListener("click", function() {
+  score = 0;
+  scoreElement.innerHTML = "Clicks: " + score;
+});
+
+window.onload = function(){
+  maxScore = localStorage.getItem("maxScore") || 0;
+  maxScoreElement.innerHTML = "Máximos Clicks: " + maxScore;
+}
+
+
+
